a07c3baceea37e5350fb3050033d8fdc7d2257f7
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2022-12-24 18:41:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,33 +14,33 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['1327973537'].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['-1001695882245'].split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('1327973537').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
+auth_channel = environ.get('-1001695882245')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
-AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
+AUTH_GROUPS = [int(admin) for admin in environ.get("-1001695882245").split()]
 
 # MongoDB information
-DATABASE_URI = environ['DATABASE_URI']
-DATABASE_NAME = environ['DATABASE_NAME']
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = environ['mongodb+srv://vpcreation:VPCREATION@atlascluster.hocwhdz.mongodb.net/?retryWrites=true&w=majority']
+DATABASE_NAME = environ['AtlasCluster']
+COLLECTION_NAME = environ.get('Telegram_files')
 
 # Messages
 default_start_msg = """
-**Hi, I'm Media Search Bot or ypu can call me as Auto-Filter Bot**
+**Hi, I'm Media Search Bot or you can call me as Auto-Filter Bot**
 Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
 
-FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
-OMDB_API_KEY = environ.get("OMDB_API_KEY", "")
+FILE_CAPTION = environ.get("")
+OMDB_API_KEY = environ.get("<b>Join [MDISK MOVIES](https://t.me/mdisk_movies_files) for more useful bots</b>\n\n<code>{file_name}</code>\nSize{file_size}\n{file_caption}.OMDB_API_KEY", "9b8a4248")
 if FILE_CAPTION.strip() == "":
     CUSTOM_FILE_CAPTION=None
 else:
     CUSTOM_FILE_CAPTION=FILE_CAPTION
-if OMDB_API_KEY.strip() == "":
+if OMDB_API_KEY.strip() == "9b8a4248":
     API_KEY=None
 else:
     API_KEY=OMDB_API_KEY
