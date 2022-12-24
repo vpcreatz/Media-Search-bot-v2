c40ee94a4487597c3ae3a01acec36861cf0a867b
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2022-12-24 18:57:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,9 +5,9 @@ id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ['20960397'])
+API_HASH = environ['d68d847d3abb2087bf74f5d0683c2993']
+BOT_TOKEN = environ['5880821820:AAGzh_tMCLtrHN6RTvPxTWAYlsE_johGbIY']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
